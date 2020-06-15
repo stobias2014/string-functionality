2ff32bf8b90058cb351a229e34f40e033ef8939f
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-15 17:39:59 -0500

    added functions

--- src/com/tobias/saul/App.java
+++ src/com/tobias/saul/App.java
@@ -5,7 +5,14 @@ public class App {
 	public static void main(String[] args) {
 		String name = "John Doe";
 		
-		
+		System.out.println(name.charAt(0));
+		System.out.println(name.concat(" Vaun"));
+		System.out.println(name);
+		System.out.println("Length: " + name.length());
+		System.out.println(name.replace('o', 'u'));
+		System.out.println(name);
+		System.out.println(name.toLowerCase());
+		System.out.println(name.toUpperCase());
 	}
 
 }
